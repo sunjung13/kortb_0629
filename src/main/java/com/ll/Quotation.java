c72a3cb7fa1669f76a42ef0dd41caa0282d815b7
sunjung13 <itclasssunjung@gmail.com>
2024-07-06 13:18:33 +0900

    2024 07 06

---
+++ src/main/java/com/ll/Quotation.java
@@ -0,0 +1,15 @@
+package com.ll;
+
+public class Quotation {
+
+    int id;
+
+    String content;
+
+    String authorName;
+
+    public Quotation(int id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+    }
+}
